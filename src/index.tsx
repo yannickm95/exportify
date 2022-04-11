c9866470eaf3758b316ef715b44dc1d6fa79c437
Yannick Maes <yannick.maes@nipro-group.com>
2022-04-11 17:46:30 +0200

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from 'components/App';
 import loadFonts from './assets/fontLoader';
 
 loadFonts();
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
