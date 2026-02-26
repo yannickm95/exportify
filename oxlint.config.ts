849193989e18c58414d930bc08e0e0dde3cf60eb
Yannick Maes <yannick.maes@nipro-group.com>
2026-02-26 17:37:47 +0100

    oxlint -> json to ts, re-add confusing-browser-globals, add import rules, auto determine react version

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,42 +1,56 @@
-{
-  "$schema": "./node_modules/oxlint/configuration_schema.json",
-  "plugins": ["eslint", "jsx-a11y", "oxc", "react", "typescript"],
-  "env": {
-    "builtin": true,
-    "es2026": true,
-    "browser": true
+import restrictedBrowserGlobals from "confusing-browser-globals";
+import { defineConfig } from "oxlint";
+
+import pkg from "./package.json" with { type: "json" };
+
+export default defineConfig({
+  plugins: ["eslint", "import", "jsx-a11y", "oxc", "react", "typescript"],
+  env: {
+    builtin: true,
+    es2026: true,
+    browser: true,
   },
-  "settings": {
-    "react": {
-      "version": "19.2.4"
-    }
+  settings: {
+    react: {
+      version: pkg.dependencies.react.replaceAll(/[^\d.]/g, ""),
+    },
   },
-  "categories": {
-    "correctness": "error",
-    "nursery": "warn"
+  categories: {
+    correctness: "error",
+    nursery: "warn",
   },
-  "ignorePatterns": ["*", "!src/**/*", "!vite.config.ts"],
-  "rules": {
+  ignorePatterns: ["*", "!src/**/*", "!vite.config.ts"],
+  rules: {
     "getter-return": "error",
     "no-array-constructor": "error",
     "no-console": "error",
     "no-case-declarations": "error",
-    "no-empty": ["error", { "allowEmptyCatch": true }],
+    "no-empty": ["error", { allowEmptyCatch: true }],
     "no-fallthrough": "error",
     "no-prototype-builtins": "error",
     "no-redeclare": "error",
     "no-regex-spaces": "error",
+    "no-restricted-globals": ["error", "setTimeout", "setInterval", "clearTimeout", "clearInterval"].concat(
+      restrictedBrowserGlobals,
+    ),
     "no-undef": "error",
     "no-unexpected-multiline": "error",
     "no-unreachable": "error",
     "no-unused-vars": [
       "error",
       {
-        "ignoreRestSiblings": true,
-        "argsIgnorePattern": "^_",
-        "destructuredArrayIgnorePattern": "^_"
-      }
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
     ],
+
+    "import/consistent-type-specifier-style": "warn",
+    "import/first": "warn",
+    "import/no-cycle": ["warn", { ignoreTypes: false }],
+    "import/no-default-export": "warn",
+    "import/no-duplicates": "warn",
+    "import/no-empty-named-blocks": "warn",
 
     "react/display-name": "error",
     "react/jsx-no-comment-textnodes": "error",
@@ -63,15 +77,20 @@
     "typescript/prefer-function-type": "error",
 
     // disable unneeded plugin enabled rules
-    "no-control-regex": "off",
-    "typescript/prefer-readonly-parameter-types": "off"
+    "no-control-regex": "allow",
+    "import/default": "allow",
+    "import/export": "allow",
+    "import/named": "allow",
+    "import/namespace": "allow",
+    "typescript/prefer-readonly-parameter-types": "allow",
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["vite.config.ts"],
-      "rules": {
-        "no-undef": "off"
-      }
-    }
-  ]
-}
+      files: ["vite.config.ts"],
+      rules: {
+        "no-undef": "allow",
+        "import/no-default-export": "allow",
+      },
+    },
+  ],
+});
