3003b896550cd76e3a94676fb250c72f1634e35a
Yannick Maes <yannick.maes@nipro-group.com>
2026-03-03 12:52:57 +0100

    remove vite-plugin-oxlint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import reactPlugin from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import oxlintPlugin from "vite-plugin-oxlint";
 import tsConfigPathsPlugin from "vite-tsconfig-paths";
 
 // @ts-expect-error
 const PORT = parseInt(process.env.PORT) || 9999;
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/exportify/",
 
   server: {
@@ -19,5 +18,5 @@ export default defineConfig(({ mode }) => ({
     port: PORT,
   },
 
-  plugins: [reactPlugin(), tsConfigPathsPlugin(), mode === "development" && oxlintPlugin()],
-}));
+  plugins: [reactPlugin(), tsConfigPathsPlugin()],
+});
